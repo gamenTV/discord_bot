09270a967b4ee8093019b9b00cca23e3e89bf580
gamenTV <pascal.roselieb@web.de>
2021-12-23 22:44:59 +0100

    remove token

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord.ext import commands, tasks
 from modules.free_games import FreeGames
 from config.config import ConfigTool
 
-token = "ODQ4ODc0NTcxNTc0NDExMjg0.YLS-DQ.7YRa-pZnZJr9kkuq3IkfczGwBGU"
+token = ""
 
 client = commands.Bot(command_prefix='.')
 ConfigFunc = ConfigTool()
