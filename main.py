c9db766aa2dff332325c8d1178f830bbdd28452a
gamenTV <pascal.roselieb@web.de>
2022-01-07 18:38:19 +0100

    Rework - new_game command - setings command - check games task
    Edit
    - Edited config.py

--- main.py
+++ main.py
@@ -1,46 +1,34 @@
 from discord import Game
-from discord.ext import commands, tasks
-from modules.free_games import FreeGames
-from config.config import ConfigTool
+from discord.ext import tasks
+from config.config import change_setting
+from modules.free_games import func_create_game, func_check_games
 from json import load
 from os.path import dirname
 from discord_components import ComponentsBot
 
 token = load(open(dirname(__file__) + "\\config\\token.json"))["token"]
-
 client = ComponentsBot(command_prefix='.')
-ConfigFunc = ConfigTool(client)
 
 
 @client.event
 async def on_ready():
     await client.change_presence(activity=Game(""))
-    free_config = await ConfigFunc.get_config("free_games")
-    free_config_val = free_config["value"]
-
-    global FreeGamesFunc
-    FreeGamesFunc = FreeGames(client, await ConfigFunc.reload_channel(),
-                              free_config_val["free_games_role"]["value"],
-                              free_config_val["free_games_poster_role"]["value"],
-                              free_config_val["embed_color"]["value"])
     check_for_expired.start()
 
 
 @client.command()
 async def setting(ctx):
-    await ConfigFunc.change_setting(ctx)
+    await change_setting(ctx, client)
 
 
 @client.command()
-async def new_game(ctx, titel, start, expires, link, price):
-    color, id = await ConfigFunc.select_color(ctx)
-    color = int(color['value'].replace("#", ""), 16)
-    await FreeGamesFunc.func_create_game(ctx, titel, start, expires, link, price, color)
+async def new_game(ctx):
+    await func_create_game(ctx, client)
     await ctx.message.delete()
 
 
 @tasks.loop(minutes=5.0)
 async def check_for_expired():
-    await FreeGamesFunc.func_check_games()
+    await func_check_games(client)
 
 client.run(token)
--- modules/free_games.py
+++ modules/free_games.py
@@ -1,67 +1,131 @@
-from discord import Game, Embed, Colour
+from discord import Game, Embed
 from os.path import dirname
+from requests import get
 from datetime import datetime
 from json import load, dump
+from config.config import get_config, select_color
+
+json_path = dirname(__file__) + "\\free_games.json"
+json_data = load(open(json_path))
 
 
-class FreeGames:
-    def __init__(self, client, channel, ping_role, creator_role, color):
-        self.client = client
-        self.json_path = dirname(__file__)+"\\free_games.json"
-        self.json_data = load(open(self.json_path))
-        self.embed_color = color
-        self.channel = channel
-        self.ping_role = ping_role
-        self.creator_role = creator_role
+def free_config(config, value):
+    return config["free_games"]["value"][value]["value"]
 
-    async def func_check_games(self):
-        delete = []
-        await self.client.change_presence(activity=Game('Check Games!'))
-        time = datetime.now().strftime("%d.%m.%Y %H:%M")
-        for game in self.json_data:
-            json_game = self.json_data[game]
-            if json_game['expires'] < time:
-                try:
-                    msg_id = json_game['message_id']
-                    msg = await self.channel.fetch_message(msg_id)
-                except Exception as e:
+
+async def func_check_games(client):
+    channel = client.get_channel(free_config(await get_config(), "free_games_channel"))
+    delete = []
+    await client.change_presence(activity=Game('Check Games!'))
+    time = datetime.now().strftime("%d.%m.%Y %H:%M")
+    for game in json_data:
+        json_game = json_data[game]
+        if json_game['expires'] < time:
+            try:
+                msg_id = json_game['message_id']
+                msg = await channel.fetch_message(msg_id)
+            except Exception as e:
+                print(e)
+                delete.append(game)
+                continue
+
+            if json_game["expires"] < time:
+                if msg:
+                    await msg.delete()
                     delete.append(game)
-                    continue
+        elif (json_game['message_id'] is None) and (json_game['start'] < time):
+            message = await send_game_msg(client, game, json_game['expires'], json_game['link'],
+                                          json_game['price'], json_game['color'])
+            json_game['message_id'] = message.id
 
-                if json_game["expires"] < time:
-                    if msg:
-                        await msg.delete()
-                        delete.append(game)
-            elif (json_game['message_id'] is None) and (json_game['start'] < time):
-                message = await self.send_game_msg(game, json_game['expires'],
-                                                   json_game['link'], json_game['price'], json_game['color'])
-                json_game['message_id'] = message.id
+    for game in delete:
+        del json_data[game]
+    else:
+        dump(json_data, open(json_path, "w"), indent=4)
 
-        for game in delete:
-            del self.json_data[game]
-        else:
-            dump(self.json_data, open(self.json_path, "w"), indent=4)
+    await client.change_presence(activity=Game("Idling!"))
 
-        await self.client.change_presence(activity=Game("Idling!"))
 
-    async def func_create_game(self, ctx, titel, start, expires, link, price, color):
-        if not color:
-            color = self.embed_color
-        roles = ctx.message.author.roles
-        if [role for role in roles if int(role.id) == self.creator_role]:
-            if datetime.strptime(expires, "%d.%m.%Y %H:%M"):
-                self.json_data[titel] = {"start": start, "expires": expires, "link": link, "price": price, "message_id": None, "color": color}
-                dump(self.json_data, open(self.json_path, "w"), indent=4)
-        else:
-            self.channel.send("You do not have Permission to do that!")
+async def func_create_game(ctx, client):
+    creator_role = free_config(await get_config(), "free_games_poster_role")
+    channel = client.get_channel(free_config(await get_config(), "free_games_channel"))
 
-    async def send_game_msg(self, titel, expires, link, price, color):
-        embed = Embed(title=titel, description=f"Expires **{expires}** Uhr",
-                      color=color)
-        embed.add_field(name="**Link**", value=f"**||{link}||**", inline=False)
-        embed.add_field(name="**Price**", value=f"**~~{price}~~€**", inline=False)
-        embed.set_footer(text="Free Games every time i post :)", icon_url="https://i.imgur.com/uZIlRnK.png")
+    data = {
+        "start": {
+            "question": "Since when is the Game Free? (Example: 01.01.2022 17:00)",
+            "type": "datetime",
+            "value": None
+        },
+        "expires": {
+            "question": "When dos the Game Expires? (Example: 01.01.2022 17:00)",
+            "type": "datetime",
+            "value": None
+        },
+        "name": {
+            "question": "Enter the game name:",
+            "type": str,
+            "value": None
+        },
+        "price": {
+            "question": "Please enter the Game Price without Currency symbol",
+            "type": float,
+            "value": None
+        }
+    }
 
-        message = await self.channel.send(f"<@&{self.ping_role}>", embed=embed)
+    ask = await channel.send("Please reply with the FreeGame Link:")
+    answer = await client.wait_for("message", check=lambda m: m.author == ctx.author)
+    await ask.delete()
+    link = answer.content
+    await answer.delete()
+    answer = link.split("/")
+    if answer[2] == "store.steampowered.com":
+        appID = answer[4]
+        response = get(f"https://store.steampowered.com/api/appdetails/?appids={appID}&cc=de%22")
+        j = response.json()
+        game_data = j["{}".format(appID)]["data"]
+        data["name"]["value"] = game_data["name"]
+        data["price"]["value"] = game_data["price_overview"]["initial_formatted"][:-1]
 
-        return message
+    print(type(data["price"]["value"]))
+
+    for d in data:
+        if data[d]["value"] is None:
+            while True:
+                ask = await ctx.send(data[d]["question"])
+                answer = await client.wait_for("message", check=lambda m: m.author == ctx.author)
+                await answer.delete()
+                await ask.delete()
+                if data[d]["type"] == "datetime":
+                    try:
+                        datetime.strptime(answer.content, "%d.%m.%Y %H:%M")
+                        data[d]["value"] = answer.content
+                        break
+                    except ValueError:
+                        data[d]["question"] = "Wrong Formatting! \n" + data[d]["question"]
+
+    color, id = await select_color(ctx, client)
+    color = int(color['value'].replace("#", ""), 16)
+
+    roles = ctx.message.author.roles
+    if [role for role in roles if int(role.id) == creator_role]:
+        json_data[data["name"]["value"]] = {"start": data["start"]["value"], "expires": data["expires"]["value"],
+                                            "link": link, "price": data["price"]["value"],
+                                            "message_id": None, "color": color}
+        dump(json_data, open(json_path, "w"), indent=4)
+    else:
+        channel.send("You do not have Permission to do that!")
+
+
+async def send_game_msg(client, titel, expires, link, price, color):
+    channel = client.get_channel(free_config(await get_config(), "free_games_channel"))
+    ping_role = free_config(await get_config(), "free_games_role")
+    embed = Embed(title=titel, description=f"Expires **{expires}** Uhr",
+                  color=color)
+    embed.add_field(name="**Link**", value=f"**||{link}||**", inline=False)
+    embed.add_field(name="**Price**", value=f"**~~{price}~~€**", inline=False)
+    embed.set_footer(text="Free Games every time i post :)", icon_url="https://i.imgur.com/uZIlRnK.png")
+
+    message = await channel.send(f"<@&{ping_role}>", embed=embed)
+
+    return message
